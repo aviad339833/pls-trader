049f03f494faf26bd888fbb8f396ef13f7658733
Lauri Peltonen <20242241+microbecode@users.noreply.github.com>
2023-08-25 18:45:41 +0300

    minor cleaning

--- scripts/trade.ts
+++ scripts/trade.ts
@@ -1,25 +1,5 @@
 import { ethers } from "hardhat";
 import pair_ABI from "./pair_ABI.json";
-/*
-function getRatio(contractAddress):
--this functoin should watch the ratio between 
-DAi = 0xefD766cCb38EaF1dfd701853BFCe31359239F305
-to 
-PLS native 
-
-
-this is the url for it:
-https://app.pulsex.com/swap?outputCurrency=0xefD766cCb38EaF1dfd701853BFCe31359239F305&chain=pulsechain
-
-so every second if possible to fetch the ratio between PLS to DAI
-
-
-Script Overview:
-
-Connect to a PulseChain node.
-Listen for new blocks.
-When a new block is detected, query the contract for the ratio.
-*/
 
 async function main() {
 
@@ -39,30 +19,21 @@ async function main() {
     provider
   );
 
-  const data = await pair_contract.getReserves();
-  console.log("DATAAA", data);
 
-  // You'd ideally want the ABI (Application Binary Interface) of the contract to interact with its methods.
-  // If it's a standard ERC-20, then there's a standard ABI you could use.
-  const ABI = [];  // Replace with actual ABI
-
-  //const contract = new web3.eth.Contract(ABI, PAIR_ADDRESS);
-
-/*   async function getRatio() {
+  async function getRatio() {
       try {
-          // This method call depends on how the contract is set up
-          // You'd typically call a function in the contract that gives you the desired ratio.
-          const ratio = await contract.methods.getYourMethodName().call();
+          const data = await pair_contract.getReserves();
+          console.log("reserves", data);
 
-          console.log(`The current ratio is: ${ratio}`);
       } catch (err) {
           console.error('Error fetching ratio:', err);
       }
-  } */
+  } 
 
   // This is a simple polling mechanism to call getRatio every second.
   // You might want to optimize it to be event-driven if the blockchain supports such events.
   //setInterval(getRatio, 10000);
+  getRatio();
 }
 
 // We recommend this pattern to be able to use async/await everywhere
