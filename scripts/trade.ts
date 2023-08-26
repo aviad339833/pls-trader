b93af56b30d7c9fb123bb2d8e45760a6bf471189
Lauri Peltonen <20242241+microbecode@users.noreply.github.com>
2023-08-26 11:43:00 +0300

    starting add trade, not working

--- scripts/trade.ts
+++ scripts/trade.ts
@@ -6,25 +6,34 @@ async function main() {
   const targetRatioDecimals = 0.0000338;
 
   const amplifier = 10000000000n; // to get rid of rounding issues
-  // max number   = 9007199254740991
+  // Number.MAX_SAFE_INTEGER   = 9007199254740991
 
   const targetRatio =  BigInt(Math.round(Number(amplifier) * targetRatioDecimals));
   
-
-  const RPC_URL = 'https://rpc.pulsechain.com';
+const hardhatWalletKey = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80';
+  const RPC_URL = 'http://127.0.0.1:8545';
+  //const RPC_URL = 'https://rpc.pulsechain.com';
 
   const DAI_ADDRESS = '0xefD766cCb38EaF1dfd701853BFCe31359239F305';
   const WPLS_ADDRESS = '0xa1077a294dde1b09bb078844df40758a5d0f9a27';
   const PAIR_ADDRESS = '0xe56043671df55de5cdf8459710433c10324de0ae';
 
-  //const provider = ethers.getDefaultProvider("pulsechain");
-  const provider = new ethers.JsonRpcProvider("https://rpc.pulsechain.com");
+  
 
-  const pair_contract = new ethers.Contract(
+  //const provider = ethers.getDefaultProvider("pulsechain");
+  const provider = new ethers.JsonRpcProvider(RPC_URL);
+  const signer = new ethers.Wallet(hardhatWalletKey, provider);
+
+   const pair_contract = new ethers.Contract(
     PAIR_ADDRESS,
     pair_ABI,
-    provider
-  );
+    signer
+  ); 
+  /* const pair_contract = new ethers.BaseContract(
+    PAIR_ADDRESS,
+    pair_ABI,
+    signer
+  ) */
 
 
   const getRatio = async () => {
@@ -48,8 +57,19 @@ async function main() {
     if (ratio && ratio > targetRatio) {
       console.log(`hurray, let's trade`);
 
+      const usevalue = 100000;
+
       const executeTrade = async () => {
         // TODO
+        await pair_contract.swapETHForExactTokens(
+          1, // amountOutMin (uint256)
+          [ethers.ZeroAddress, DAI_ADDRESS], // path (address[])
+          signer.address, // to (address)
+          4317403, // deadline (uint256)
+          usevalue, // value (uint256)
+          { value: usevalue }
+        )
+
       }
 
       await executeTrade();
