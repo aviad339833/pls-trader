b88917229db685d2407345797fb8104aa8bebbf9
Lauri Peltonen <20242241+microbecode@users.noreply.github.com>
2023-08-27 17:09:48 +0300

    two-way trading

--- scripts/trade.ts
+++ scripts/trade.ts
@@ -2,28 +2,35 @@ import { ethers } from "hardhat";
 import pair_ABI from "./pair_ABI.json";
 import router_ABI from "./router_ABI.json";
 import wpls_ABI from "./wpls_ABI.json";
+import { setNonce } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 
 async function main() {
-  // The minimum ratio
-  const targetRatioDecimals = 0.0000338;
+  // The three most important variables
+  const targetRatioDecimalsPLStoDai = 0.0000338;
+  const targetRatioDecimalsDaiToPLS = 28745;
+  const tradePLStoDAI = false; // true if trading PLS to DAI, false if DAI to PLS
+
+  // two next important variables
+  const hardhatWalletKey = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80';
+  const RPC_URL = 'http://127.0.0.1:8545';
+  //const RPC_URL = 'https://rpc.pulsechain.com';
 
   const amplifier = 10000000000n; // to get rid of rounding issues
   // Number.MAX_SAFE_INTEGER   = 9007199254740991
 
-  const targetRatio =  BigInt(Math.round(Number(amplifier) * targetRatioDecimals));
-  
-const hardhatWalletKey = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80';
-  const RPC_URL = 'http://127.0.0.1:8545';
-  //const RPC_URL = 'https://rpc.pulsechain.com';
+  let targetRatio : BigInt;
+  if (tradePLStoDAI) {
+    targetRatio =  BigInt(Math.round(Number(amplifier) * targetRatioDecimalsPLStoDai));
+  }
+  else {
+    targetRatio =  BigInt(Math.round(Number(amplifier) * targetRatioDecimalsDaiToPLS));
+  }
 
   const DAI_ADDRESS = '0xefD766cCb38EaF1dfd701853BFCe31359239F305';
   const WPLS_ADDRESS = '0xa1077a294dde1b09bb078844df40758a5d0f9a27';
   const PAIR_ADDRESS = '0xe56043671df55de5cdf8459710433c10324de0ae';
   const ROUTER_ADDRESS = '0x98bf93ebf5c380C0e6Ae8e192A7e2AE08edAcc02';
 
-  
-
-  //const provider = ethers.getDefaultProvider("pulsechain");
   const provider = new ethers.JsonRpcProvider(RPC_URL);
   const signer = new ethers.Wallet(hardhatWalletKey, provider);
 
@@ -32,7 +39,7 @@ const hardhatWalletKey = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae78
     pair_ABI,
     signer
   ); 
-  const router_contract = new ethers.Contract(
+  let router_contract = new ethers.Contract(
     ROUTER_ADDRESS,
     router_ABI,
     signer
@@ -49,10 +56,15 @@ const hardhatWalletKey = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae78
   ); 
 
   const getRatio = async () => {
-      try {
-          
+      try {          
           const reserves = await pair_contract.getReserves();
-          const ratio = (amplifier * reserves[1]) / reserves[0];
+          let ratio : BigInt;
+          if (tradePLStoDAI) {
+            ratio = (amplifier * reserves[1]) / reserves[0];
+          }
+          else {
+            ratio = (amplifier * reserves[0]) / reserves[1];
+          }
 
           return ratio;
 
@@ -65,32 +77,52 @@ const hardhatWalletKey = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae78
   const tradeIfRatio = async () => {
     const ratio = await getRatio();
     console.log(`Ratio: ${ratio}, targetRatio: ${targetRatio}`);
-    if (ratio && ratio > targetRatio) {
-      console.log(`hurray, let's trade`);
-
-      const usevalue = 100000000;
+    if (ratio && 
+      ((tradePLStoDAI && ratio > targetRatio) || 
+      (!tradePLStoDAI && ratio < targetRatio))) {
+      console.log(`hurray, let's trade, direction: `, tradePLStoDAI);
 
       // Get the blockchain's timestamp
       const block = await provider.getBlock('latest');
       const timestamp = block!.timestamp;
       const deadline = timestamp + (1000); // add a bit of extra time before deadline. in seconds
-      console.log('timestamp', timestamp, deadline);
+      //console.log('timestamp', timestamp, deadline);
 
       const oldWPLSBalance = await wpls_contract.balanceOf(signer.address);
       const oldDaiBalance = await dai_contract.balanceOf(signer.address);
       const oldNativeBalance = await provider.getBalance(signer.address);
 
       const executeTrade = async () => {
-        await router_contract.swapETHForExactTokens(
-          10, // amountOutMin (uint256)
-          [WPLS_ADDRESS, DAI_ADDRESS], // path (address[])
-          signer.address, // to (address)
-          deadline, // deadline (uint256)
-          { value: usevalue }
-        )
-      }
+        if (tradePLStoDAI) {
+          const inputPLS = oldNativeBalance / 100n * 97n; // leave 3% for gas costs
+          await router_contract.swapExactETHForTokens(
+            1, // amountOutMin (uint256) (slippage)
+            [WPLS_ADDRESS, DAI_ADDRESS], // path (address[])
+            signer.address, // to (address)
+            deadline, // deadline (uint256)
+            { value: inputPLS }
+          )
+        }
+        else {
+          if (oldDaiBalance == 0) {
+            throw "No DAI to trade";
+          }
+          await dai_contract.approve(ROUTER_ADDRESS, oldDaiBalance);
 
-      
+          // Unsure why we have to manually adjust the nonce
+          const nonce = await signer.getNonce();
+
+          await router_contract.swapExactTokensForETH(
+            oldDaiBalance, // amountIn (uint256)
+            1, // amountOutMin (uint256) (slippage)
+            [DAI_ADDRESS, WPLS_ADDRESS], // path (address[])
+            signer.address, // to (address)
+            deadline, // deadline (uint256)
+            { nonce: nonce }
+          )
+        }
+        
+      }
 
       await executeTrade();
 
@@ -101,9 +133,12 @@ const hardhatWalletKey = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae78
       console.log(`WPLS balances. Old: ${oldWPLSBalance}. New: ${newWPLSBalance}. Diff: ${newWPLSBalance - oldWPLSBalance}` );
       console.log(`DAI balances. Old: ${oldDaiBalance}. New: ${newDaiBalance}. Diff: ${newDaiBalance - oldDaiBalance}` );
       console.log(`Native balances. Old: ${oldNativeBalance}. New: ${newNativeBalance}. Diff: ${newNativeBalance - oldNativeBalance}` );
+
+      return true;
     }
     else {
       console.log(`shitty price, let's not trade`);
+      return false;
     }
   }
 
