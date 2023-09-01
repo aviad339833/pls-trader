532907a733a5cbc172a1efa6682c6965210aa104
aviad339833 <aviad339833@gmail.com>
2023-09-01 14:50:17 -0400

    trendline alerts

--- scripts/ratios.ts
+++ scripts/ratios.ts
@@ -12,12 +12,25 @@ db.run(`
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     token TEXT,
     ratio TEXT,
+    ma_120 TEXT,
+    ma_120_ago TEXT,
     timestamp TEXT
   )
 `);
 
 const historicalRatios: Record<string, BigInt[]> = {};
 let runCounter = 0;
+
+function calculateMovingAverage(values: any) {
+  const period = 120;
+  // Calculate the sum of the firast 120 values
+  const sum = values
+    .slice(0, period)
+    .reduce((total: any, value: any) => total + value, 0n);
+
+  // Calculate the moving average for the entire period (including the first price)
+  return sum / BigInt(period);
+}
 
 async function updateRatios() {
   try {
@@ -46,20 +59,39 @@ async function updateRatios() {
 
     const timestamp = new Date().toISOString();
     for (const [token, ratio] of Object.entries(ratios)) {
-      db.run(
-        `INSERT INTO token_prices (token, ratio, timestamp) VALUES (?, ?, ?)`,
-        [token, ratio?.toString(), timestamp],
-        (error) => {
-          if (error) {
-            console.error("Error inserting data into the database:", error);
-          } else {
-            console.log("Inserted data into the database:");
-            console.log("Token:", token);
-            console.log("Ratio:", ratio?.toString());
-            console.log("Timestamp:", timestamp);
-          }
-        }
+      const movingAverage = calculateMovingAverage(historicalRatios[token]);
+      const movingAverage120Ago = calculateMovingAverage(
+        historicalRatios[token].slice(-240, -120)
       );
+
+      if (movingAverage !== null && movingAverage120Ago !== null) {
+        // Update the ma_120 column in the database
+        db.run(
+          `INSERT INTO token_prices (token, ratio, ma_120, ma_120_ago, timestamp) VALUES (?, ?, ?, ?, ?)`,
+          [
+            token,
+            ratio?.toString(),
+            movingAverage.toString(),
+            movingAverage120Ago.toString(),
+            timestamp,
+          ],
+          (error) => {
+            if (error) {
+              console.error("Error inserting data into the database:", error);
+            } else {
+              console.log("Inserted data into the database:");
+              console.log("Token:", token);
+              console.log("Ratio:", ratio?.toString());
+              console.log("Moving Average (120):", movingAverage.toString());
+              console.log(
+                "Moving Average (120 ago):",
+                movingAverage120Ago.toString()
+              );
+              console.log("Timestamp:", timestamp);
+            }
+          }
+        );
+      }
     }
 
     console.log("Data inserted into the database.");
--- scripts/trade.ts
+++ scripts/trade.ts
@@ -1,62 +1,103 @@
-import { ethers } from "hardhat";
-import pair_ABI from "../abis/pair_ABI.json";
-import router_ABI from "../abis/router_ABI.json";
-import wpls_ABI from "../abis/wpls_ABI.json";
-import { sendMessage } from "../utils/sendMessage";
-import { addresses } from "../config/config";
-import { gatherUserInputs } from "../utils/userInputs";
-import { getRatio } from "../utils/getRatio";
-import { getRatiosFromJson } from "../utils/getRatiosFromJson";
-import { executeTrade } from "../utils/takeAtrade";
+import fs from "fs";
 import { readLast3Items } from "../sqlLIte/readLast3Items";
+// Import other necessary modules here
 
-require("dotenv").config();
-const fs = require("fs");
-// Check ratio and execute trade if conditions met
+type Balances = {
+  [key: string]:
+    | number
+    | {
+        highestPercentageDifferences: number;
+        lowestPercentageDifferences: number;
+      };
+};
 
-function readJsonFromFile(filename: any) {
+function readJsonFromFile(filename: string): Balances | null {
   try {
     const jsonString = fs.readFileSync(filename, "utf8");
     const jsonData = JSON.parse(jsonString);
     return jsonData;
   } catch (error) {
-    console.error(`Error reading or parsing JSON data from '${filename}`);
+    console.error(`Error reading or parsing JSON data from ${filename}`);
     return null;
   }
 }
-// ... (other imports and code above)
+
+let flagOpposite = false; // Add this flag to switch the behavior
+const calculateTrendLine = (entryLevel: number, slopeDegree: number) => {
+  const slope = Math.tan(
+    (flagOpposite ? -slopeDegree : slopeDegree) * (Math.PI / 180)
+  );
+  return (time: number) => entryLevel + slope * time;
+};
+
+let time = 0;
+const entryLevel = 0.0000374;
+const slopeDegree = 80; // Changed to 50 degrees as per your requirement
+const trendLineFunction = calculateTrendLine(entryLevel, slopeDegree);
+let stopLossLevel = entryLevel * (1 - 0.5 / 100); // Initial stop loss level
+const targetToken = "DAI"; // Token to target
 
 async function checkAndExecuteTrade() {
   try {
-    const ratios = await readLast3Items();
-    const balances = await readJsonFromFile("balances.json");
+    const ratios = await readLast3Items(); // Implement the actual function
+    const balances: Balances | null = readJsonFromFile("balances.json");
+
+    if (!balances) return;
+
     console.clear();
-    console.log("Balances JSON:");
-    console.log(balances);
-    console.log("Ratios Array:");
-    console.log(ratios);
 
-    // Log individual properties of balances
-    console.log("Individual Balances:");
-    for (const key in balances) {
-      console.log(`${key}: ${balances[key]}`);
+    console.log(`=== Current Time: ${new Date().toLocaleTimeString()} ===`); // Added line
+
+    console.log(`=== Trend Line Info ===`);
+    console.log(
+      `Trend Line Status: ${flagOpposite ? "Going Down" : "Going Up"}`
+    );
+    console.log(`Trend Line Degree: ${slopeDegree} degrees`);
+
+    console.log(`Current Stop Loss Level: ${stopLossLevel.toFixed(8)}`);
+
+    console.log("\n=== Account Balances ===");
+    if (balances[targetToken]) {
+      console.log(
+        `${targetToken}: ${balances[targetToken]} (Token: ${targetToken})`
+      );
     }
 
-    // Log individual properties of highestPercentageDifferences
-    console.log("Highest Percentage Differences:");
-    for (const key in balances.highestPercentageDifferences) {
-      console.log(`${key}: ${balances.highestPercentageDifferences[key]}`);
+    console.log("\n=== Ratios ===");
+    for (const ratio of ratios) {
+      if (ratio.token !== targetToken) continue;
+
+      const currentRatio = ratio.ratio;
+      const trendLineValue = trendLineFunction(time);
+      const distancePercentage =
+        ((trendLineValue - currentRatio) / trendLineValue) * 100;
+
+      const isBelowTrendLine = flagOpposite
+        ? currentRatio > trendLineValue
+        : currentRatio < trendLineValue;
+      const isBelowStopLoss = currentRatio < stopLossLevel;
+
+      console.log(`Token: ${ratio.token}, Ratio: ${currentRatio}`);
+
+      if (isBelowTrendLine || isBelowStopLoss) {
+        console.log(
+          `Ratio ${
+            flagOpposite ? "broke above" : "broke below"
+          } the trend line or hit the stop loss.`
+        );
+        // Adjust stop loss level
+        stopLossLevel = currentRatio * (1 - 0.5 / 100);
+      } else {
+        console.log(`trendLineValue`, trendLineValue);
+        console.log(`Did not break the trend line yet.`);
+      }
     }
 
-    // Log individual properties of lowestPercentageDifferences
-    console.log("Lowest Percentage Differences:");
-    for (const key in balances.lowestPercentageDifferences) {
-      console.log(`${key}: ${balances.lowestPercentageDifferences[key]}`);
-    }
+    time++;
   } catch (error) {
     console.error("An error occurred:", error);
   }
 }
 
-// Poll every 5 seconds (customize this)
-setInterval(checkAndExecuteTrade, 1000); // Poll every 5 seconds
+// Poll every 1 second (customize this)
+setInterval(checkAndExecuteTrade, 1000);
